7149c2ee27e66cd1258199fd90b6dae3f395b508
h.bourget <hugobourget@protonmail.com>
2021-10-20 08:55:22 +0200

    created file: dicothomy.c / dicothomy.h

---
+++ headers/dicothomy.h
@@ -0,0 +1,17 @@
+//
+// Created by h.bourget on 20/10/2021.
+//
+
+#ifndef PROGRAMMATIONDYNAMIQUE_DICOTHOMY_H
+#define PROGRAMMATIONDYNAMIQUE_DICOTHOMY_H
+
+/**
+ * Recherche par dichotomie dans un tableau d'entiers
+ * @param array Le tableau de valeurs
+ * @param size_t La taille du tableau
+ * @param value La valeur à trouver
+ * @return The position of the value found or -1
+ */
+int find_by_dichotomy(int array[], int size_t, int value );
+
+#endif //PROGRAMMATIONDYNAMIQUE_DICOTHOMY_H
---
+++ sources/dicothomy.c
@@ -0,0 +1,8 @@
+//
+// Created by h.bourget on 20/10/2021.
+//
+
+int find_by_dichotomy(int array[], int size_t, int value )
+{
+
+}
